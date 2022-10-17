032d110221d1d4751d1b0fdf544db6aff522f024
Mariano <marianonunezma@gmail.com>
2022-10-17 14:37:45 -0300

    Adding Front Files

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 let persons = require('./persons.js');
 
+app.use(cors());
+
+app.use(express.static('build'))
 app.use(express.json());
 
 app.get('/api/persons', (req, res) => {
@@ -21,15 +25,32 @@ app.post('/api/persons', (req, res) => {
   const id = parseInt(Math.random() * 100);
   const { name, number } = req.body;
 
-  if(!name || !number) return res.status(404).send('Name and Number is Required')
+  if (!name || !number)
+    return res.status(404).send('Name and Number is Required');
 
-  const isUnique = persons.find(person => person.name === name);
+  const isUnique = persons.find((person) => person.name === name);
 
-  if(isUnique) return res.status(404).send('Name Must be Unique');
+  if (isUnique) return res.status(404).send('Name Must be Unique');
 
   const newPerson = { name, number, id };
 
   persons = [...persons, newPerson];
+
+  res.send(persons);
+});
+
+app.put('/api/persons/:id', (req, res) => {
+  const { number} = req.body;
+  const id = Number(req.params.id);
+  const person = persons.find((elem) => elem.id === id);
+
+  if (!person) return res.status(404).send('Person with this ID, Doesnt Exist');
+
+  const updatePerson = {
+    ...person, number: number 
+  }
+
+  persons = persons.map((elem) => elem.id !== id ? elem : updatePerson);
 
   res.send(persons);
 });
@@ -45,13 +66,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.send(persons);
 });
 
-app.get('/info', (req, res) => {
-  res.send(
-    `Phone book has info for ${persons.length} people<br><br>${new Date()}`
-  );
-});
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
